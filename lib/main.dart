8685907a972396192b5935569d30e0ff91f0e5ad
marcelinhoPlayer <114264445+marcelinhoPlayer@users.noreply.github.com>
2022-09-23 19:37:58 -0300

    Delete lib directory

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:clone_uber2/pages/root_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: RootApp(),
-  ));
-}
